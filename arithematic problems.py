32625efd94981583a94375090c11f424da7599bb
Gayatri Jagtap <jagtapgayatri000@gmail.com>
2025-06-09 00:14:11 +0530

    Add files via upload
    added two practice files of day 1 and day 2

---
+++ arithematic problems.py
@@ -0,0 +1,14 @@
+#all the most used operations 
+
+a = 10
+b = 2
+sum = a+b 
+diff = a-b 
+product = a*b
+division = a/b 
+remainder = a%b 
+print(sum)
+print(diff)
+print(product)
+print(division)
+print(remainder)
---
+++ conditional statements.py
@@ -0,0 +1,57 @@
+# #Grade students based on marks
+# marks = int(input("Enter marks: "))
+# if marks >= 90:
+#     print("Grade A")
+# elif 90> marks >= 80:
+#     print("Grade B")
+# elif 80 > marks >= 70:
+#     print("Grade C")
+# elif 70 > marks:
+#     print("Grade D")
+
+
+
+# #check if a number entered by the user is odd or even.
+# num = int(input("Enter a number: "))
+# if num%2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+    
+    
+# #find the greatest of 3 numbers entered by the user.
+# n1 = int(input("Enter first number: "))
+# n2 = int(input("Enter second number: "))
+# n3 = int(input("Enter third number: "))
+
+# if n1>n2 and n1>n3:
+#     print("greatest: number 1 ", n1)
+# elif n2>n3 and n2>n1:
+#     print("greatest: number 2 ", n2)
+# elif n3>n2 and n3>n1:
+#     print("greatest: number 3 ", n3)
+    
+    
+
+# #if a number is a multiple of 7 or not.
+# n = int(input ("enter a number: "))
+# if n % 7 == 0:
+#     print("multiple of 7")
+# else:
+#     print("not a multiple of 7")
+
+
+
+#discount
+age = int(input("enter your age: "))
+if age>=60:
+    mem = input("if you are a memeber input 1 and if not input 2 : ")
+    if mem==1:
+            print("30 percent senior discount")
+    else:
+        print("20 percent senior discount")
+else:
+    print("you are not a senior")
+    
+    
